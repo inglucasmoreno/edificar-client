1fcf7b909926fe3ef416f7bea950dde23d9ca695
Lucas Omar Moreno <ing.lucasmoreno@gmail.com>
2021-04-23 19:37:30 -0300

    23/05/2021 - Arreglando membrete

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,5 @@ export const environment = {
   // base_url: 'http://190.107.118.137:3000/api'
   // base_url: 'https://edificar-srl.herokuapp.com/api'
   base_url: 'http://68.233.247.52:3000/api'
+  // base_url: 'http://69.46.15.100:3000/api'
 };
